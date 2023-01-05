fe4dc29485464b65cb4b16b7386cb818c598c931
zeshan <zeshan.tawa98@gmail.com>
2023-01-05 14:44:48 -0500

    Excercise 3 complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
+const reverseString = function(string) {
+  let stringArray = string.split('');
+  let reversedArray = stringArray.reverse();
+  let newString = reversedArray.join('');
 
+  return newString;
 };
 
 // Do not edit below this line
