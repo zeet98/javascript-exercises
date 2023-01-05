7c65bf75c4984a72b82753d390fe4b935915608d
zeshan <zeshan.tawa98@gmail.com>
2023-01-05 14:19:44 -0500

    Excercise 2 complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+  let newString = '';
+  if(num > 0){
+    for(let i = 0; i < num; i++){
+      newString += string;
+    }
+    return newString; 
+  } else if(num === 0){
+    return '';
+  } else return 'ERROR';
 };
 
 // Do not edit below this line
