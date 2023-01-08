d40ceccc1b73193ca6fd2748c86b6c99e05430c4
zeshan <zeshan.tawa98@gmail.com>
2023-01-08 18:08:35 -0500

    Continued completion of excercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,7 @@
-const removeFromArray = function() {
-
-};
+let removeFromArray = function(...args) {
+    const array = args[0]
+    return array.filter(val => !args.includes(val))
+    }
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempFar) {
+  let newTemp = ((tempFar - 32) * (5/9));
+  return Math.round(newTemp * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempCelc) {
+  let newTemp = ((tempCelc * (9/5)) + 32);
+  return Math.round(newTemp * 10) / 10;
 };
 
 // Do not edit below this line
